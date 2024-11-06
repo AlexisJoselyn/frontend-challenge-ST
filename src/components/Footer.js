065f1b784a4284a3a922d9f8da128f8a4619578a
AlexisJoselyn <pfernandez.alexis@gmail.com>
2024-11-06 16:25:08 -0500

    feat:agregando componentes header y footer responsivos

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
+import logo_R from '../assets/log_R-white.svg'
+import logo_mobile from '../assets/logo_mobile_white.svg'
+import { getByDisplayValue } from "@testing-library/react";
 
 export const Footer = () => {
   return (
-    <div>
-        <p>Soy el footer</p>
-    </div>
-  )
-}
+    <footer className="footer">
+      <img className="footer__logo" src={logo_R} alt="logo" />
+      <img className="footer__logo-mobile" src={logo_mobile} alt="logo"/>
+      <p className="footer__text">© 2024 RIMAC Seguros y Reaseguros.</p>
+    </footer>
+  );
+};
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,18 @@
 import React from 'react'
+import logo_R from '../assets/logo_R.svg'
+import phone from '../assets/phone.svg'
 
 export const Header = () => {
   return (
-    <div>Header</div>
+    <header className='header'>
+      <img className='header__logo' src={logo_R} alt='logo'/>
+      <div className='header__content'>
+        <p className='content__text'>¡Comprar por este medio!</p>
+        <div className='content__phone'>
+          <img src={phone} alt='icon-phone'/>
+          <p>(01) 411 6001</p>
+        </div>
+      </div>
+    </header>
   )
 }
