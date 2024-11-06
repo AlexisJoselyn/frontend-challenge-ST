64a25292705adf2a8051f78250e27c7476374088
AlexisJoselyn <pfernandez.alexis@gmail.com>
2024-11-06 18:39:22 -0500

    chore: agregando fondos blur

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import './App.scss';
-import { Footer } from './components/footer/Footer';
-import { Header } from './components/header/Header';
-import { Home } from './components/home/Home';
+import "./App.scss";
+import { Footer } from "./components/footer/Footer";
+import { Header } from "./components/header/Header";
+import { Home } from "./components/home/Home";
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Home />
+      <div className="app__content">
+        <Header />
+        <Home />
+      </div>
       <Footer />
     </div>
   );
