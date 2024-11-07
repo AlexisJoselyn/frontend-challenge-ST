045db40a271dc195d04dfb0f82f7d3eb449d2f79
AlexisJoselyn <pfernandez.alexis@gmail.com>
2024-11-07 00:40:43 -0500

    chore:agregando formulario políticas y botón

--- src/components/home/InfoSection.js
+++ src/components/home/InfoSection.js
@@ -15,24 +15,34 @@ export const InfoSection = () => {
         </p>
       </div>
       <div className="infoSection__form">
-        <form>
-          <div className="form__document">
-            <select id="mySelect" name="options">
-              <option value="dni">DNI</option>
-              <option value="ruc">RUC</option>
-              <option value="ce">CNE</option>
-            </select>
-            <div className="document__input-container">
-              <input id="documentNumber" placeholder="" />
-              <label for="documentNumber">Nro. de documento</label>
-            </div>
+        <div className="form__document">
+          <select id="mySelect" name="options">
+            <option value="dni">DNI</option>
+            <option value="ruc">RUC</option>
+            <option value="ce">CNE</option>
+          </select>
+          <div className="document__input-container">
+            <input id="documentNumber" placeholder="" />
+            <label for="documentNumber">Nro. de documento</label>
           </div>
-          <div className="form__cellphone">
-            <input id="cellphoneNumber" placeholder="" />
-            <label for="cellphoneNumber">Celular</label>
-          </div>
-        </form>
+        </div>
+        <div className="form__cellphone">
+          <input id="cellphoneNumber" placeholder="" />
+          <label for="cellphoneNumber">Celular</label>
+        </div>
       </div>
+      <div className="infoSection__check">
+          <label>
+            <input type="checkbox"/>
+            Acepto la Política de Privacidad
+          </label>
+          <label>
+            <input type="checkbox"/>
+            Acepto la Política de Comunicaciones Comerciales
+          </label>
+          <a href="./"> Aplican Términos y Condiciones</a>
+      </div>
+      <button className="infoSection__button">Cotiza aquí</button>
     </div>
   );
 };
