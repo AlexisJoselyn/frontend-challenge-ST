e65eb41ca45c59ef6d18dbbf7c6048bbcb828a94
AlexisJoselyn <pfernandez.alexis@gmail.com>
2024-11-10 16:47:09 -0500

    feat: agregando componente volver

--- src/components/plans/BtnVolver.js
+++ src/components/plans/BtnVolver.js
@@ -1,7 +1,14 @@
 import React from 'react'
+import iconButton from '../../assets/icon_button.png'
+import './BtnVolver.scss'
 
 export default function BtnVolver() {
   return (
-    <div className='btnVover'>BtnVolver</div>
+    <div className='volver'>
+        <a className='volver__btn' href='/'>
+        <img src={iconButton} alt=''/>
+        <span>Volver</span>
+    </a>
+    </div>
   )
 }
--- src/components/plans/Plans.js
+++ src/components/plans/Plans.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Stepper from './Stepper'
+import BtnVolver from './BtnVolver'
 
 export const Plans = () => {
   return (
     <div className='plans'>
         <Stepper></Stepper>
+        <BtnVolver></BtnVolver>
     </div>
   )
 }
