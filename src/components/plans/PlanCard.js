dccd4825347626fe201a3510ad6c7a67d92a468d
AlexisJoselyn <pfernandez.alexis@gmail.com>
2024-11-10 21:59:43 -0500

    feat: agregando componente PlanSelector y PlanCard con estilos

---
+++ src/components/plans/PlanCard.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./PlanCard.scss";
+import radioButton from "../../assets/atoms_radio_button_web.png";
+
+export default function PlanCard({ icon, title, description }) {
+  return (
+    <div className="plan-card">
+      <label className="plan-card__label">
+        <input className="plan-card__option" type="radio" name="plan" />
+        <span className="plan-card__custom-radio"></span>
+      </label>
+      <div className="plan-card__info">
+        <img src={icon} alt="" />
+        <p className="info__title">{title}</p>
+        <p className="info__description">{description}</p>
+      </div>
+    </div>
+  );
+}
--- src/components/plans/Plans.js
+++ src/components/plans/Plans.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import Stepper from './Stepper'
 import BtnVolver from './BtnVolver'
+import PlanSelector from './PlanSelector'
+import './Plans.scss'
 
 export const Plans = () => {
   return (
     <div className='plans'>
         <Stepper></Stepper>
         <BtnVolver></BtnVolver>
+        <PlanSelector></PlanSelector>
     </div>
   )
 }
